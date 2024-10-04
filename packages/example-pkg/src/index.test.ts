81fdd658e07dd8e6e1300326509021ed27b216df
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-10-04 10:41:05 -0400

    chore: test

--- packages/example-pkg/src/index.test.ts
+++ packages/example-pkg/src/index.test.ts
@@ -3,7 +3,7 @@ import { type User, greetUser } from './index.js'
 describe('greet user tests', () => {
   const user: User = {
     email: 'bob@loblaw.com',
-    first_name: 'Bob',
+    first_name: 'Bobb',
     is_admin: false,
     last_name: 'Loblaw',
   }
@@ -12,13 +12,13 @@ describe('greet user tests', () => {
     expect.assertions(1)
 
     user.is_admin = true
-    expect(greetUser(user)).toBe('Hello, Bob Loblaw! You are an admin.')
+    expect(greetUser(user)).toBe('Hello, Bobb Loblaw! You are an admin.')
   })
 
   it('should greet user as a non-admin', () => {
     expect.assertions(1)
 
     user.is_admin = false
-    expect(greetUser(user)).toBe('Hello, Bob Loblaw! You are not an admin.')
+    expect(greetUser(user)).toBe('Hello, Bobb Loblaw! You are not an admin.')
   })
 })
